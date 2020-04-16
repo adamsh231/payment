cc4ed557b67a8f26af6cf739ee7efadb66adf284
Adam Syarif Hidayatullah <adamsyarif217@yahoo.com>
2020-04-17 05:06:04 +0700

    Create Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('admin')->insert([
+            'id' => '100',
+            'username' => 'admin',
+            'password' => bcrypt('admin')
+        ]);
+        DB::table('jabatan')->insert([
+            'id' => '100',
+            'name' => 'supervisor',
+            'daily' => '120000',
+            'overtime' => '30000',
+            'food' => '50000',
+            'transport' => '10000',
+        ]);
+        DB::table('karyawan')->insert([
+            'id' => '100',
+            'jabatan_id' => '100',
+            'name' => 'Adam Syarif Hidayatullah',
+            'username' => 'adamsh231',
+            'password' => bcrypt('dayung231'),
+            'birth' => '1998-10-13',
+            'phone' => '082140320499',
+            'gender' => 1,
+        ]);
     }
 }
