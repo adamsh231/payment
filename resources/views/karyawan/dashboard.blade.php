488bbd1f5ae7518d8358f046f1168657115cedd2
Adam Syarif Hidayatullah <adamsyarif217@yahoo.com>
2020-04-17 18:53:18 +0700

    KarV v1

--- resources/views/karyawan/dashboard.blade.php
+++ resources/views/karyawan/dashboard.blade.php
@@ -1,1 +1,88 @@
-DASHBOARD KARYAWAN
+@extends('layout/karyawan')
+@section('title', 'Dashboard')
+@section('content')
+<h1>Waroeng Super Sambal "SS"</h1>
+<div class="card shadow">
+    <div class="card-header py-3">
+        <p class="text-center text-primary m-0 font-weight-bold">Biodata</p>
+    </div>
+    <div class="card-body">
+        <form>
+            <div class="form-group">
+                <label for="address">
+                    <strong>Nama</strong>
+                </label>
+                <input class="form-control" type="text" placeholder="Nama Karyawan" name="address">
+            </div>
+            <div class="form-group">
+                <label for="address">
+                    <strong>Alamat</strong>
+                </label>
+                <input class="form-control" type="text" placeholder="Alamat Karyawan" name="address">
+            </div>
+            <div class="form-row">
+                <div class="col">
+                    <div class="form-group">
+                        <label for="city">
+                            <strong>Tanggal Lahir</strong>
+                            <br>
+                        </label>
+                        <input class="form-control" type="text" name="city">
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-group">
+                        <label for="country">
+                            <strong>No Handphone</strong>
+                        </label>
+                        <input class="form-control" type="text" placeholder="Nomor Handphone Karyawan" name="country">
+                    </div>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="col">
+                    <div class="form-group">
+                        <label for="city">
+                            <strong>Jenis Kelamin</strong>
+                        </label>
+                        <input class="form-control" type="text" placeholder="Jenis Kelamin Karyawan" name="city">
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-group">
+                        <label for="country">
+                            <strong>Mulai Bekerja</strong>
+                        </label>
+                        <input class="form-control" type="text" name="country">
+                    </div>
+                </div>
+            </div>
+            <div class="form-group">
+                <button class="btn btn-primary btn-sm float-right" data-toggle="tooltip" data-bs-tooltip="" type="submit" title="Klik untuk Merubah">Save&nbsp;Settings</button>
+            </div>
+        </form>
+    </div>
+</div>
+<div class="card shadow border-left-warning py-2" style="margin-left: 15%;margin-right: 15%;">
+    <div class="card-body">
+        <div class="row align-items-center no-gutters">
+            <div class="col mr-2">
+                <div class="text-uppercase text-center text-warning font-weight-bold text-xs mb-1">
+                    <span>PRESENSI</span>
+                </div>
+                <div class="text-center text-dark font-weight-bold h5 mb-0">
+                    <span>12 OKTOBER 2019</span>
+                </div>
+            </div>
+            <div class="col-auto">
+                <a class="btn btn-success btn-circle ml-1" role="button">
+                    <i class="fas fa-check text-white"></i>
+                </a>
+                <a class="btn btn-danger btn-circle ml-1" role="button" data-toggle="tooltip" data-bs-tooltip="" title="Klik untuk Hadir">
+                    <i class="fa fa-hand-stop-o text-white"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/layout/karyawan.blade.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}">
+    @yield('add_style')
+</head>
+
+<body id="page-top">
+    <div id="wrapper">
+        <nav class="navbar navbar-dark align-items-start sidebar sidebar-dark accordion bg-gradient-primary p-0">
+            <div class="container-fluid d-flex flex-column p-0">
+                <a class="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0" href="#">
+                    <div class="sidebar-brand-icon rotate-n-15">
+                        <i class="fas fa-laugh-wink"></i>
+                    </div>
+                    <div class="sidebar-brand-text mx-3"><span>KARYAWAN</span></div>
+                </a>
+                <hr class="sidebar-divider my-0">
+                <ul class="nav navbar-nav text-light" id="accordionSidebar">
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="index.html">
+                            <i class="fas fa-tachometer-alt"></i>
+                            <span>&nbsp;Dashboard</span></a>
+                    </li>
+                    <hr class="sidebar-divider">
+                    <div class="sidebar-heading">
+                        <p class="mb-0">Main Menu</p>
+                    </div>
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link" href="charts.html">
+                            <i class="fa fa-paste"></i><span>&nbsp;Biodata</span>
+                        </a></li>
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link" href="tables.html">
+                            <i class="fa fa-hand-paper-o"></i>
+                            <span>&nbsp;Absen</span>
+                        </a>
+                        <a class="nav-link" href="tables.html">
+                            <i class="fa fa-fire"></i>
+                            <span>&nbsp;Lembur</span>
+                        </a>
+                    </li>
+                    <hr class="sidebar-divider">
+                </ul>
+                <div class="text-center d-none d-md-inline">
+                    <button class="btn rounded-circle border-0" id="sidebarToggle" type="button"></button>
+                </div>
+            </div>
+        </nav>
+        <div class="d-flex flex-column" id="content-wrapper">
+            <div id="content">
+                <nav class="navbar navbar-light navbar-expand bg-white shadow mb-4 topbar static-top">
+                    <div class="container-fluid">
+                        <button class="btn btn-link d-md-none rounded-circle mr-3" id="sidebarToggleTop" type="button">
+                            <i class="fas fa-bars"></i>
+                        </button>
+                        <ul class="nav navbar-nav flex-nowrap ml-auto">
+                            <div class="d-none d-sm-block topbar-divider"></div>
+                            <li class="nav-item dropdown no-arrow" role="presentation">
+                                <div class="nav-item dropdown no-arrow">
+                                    <a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#">
+                                        <span class="d-none d-lg-inline mr-2 text-gray-600 small">Valerie Luna</span>
+                                        <img class="border rounded-circle img-profile" src="{{ asset('assets/img/avatars/avatar1.jpeg') }}">
+                                    </a>
+                                    <div class="dropdown-menu shadow dropdown-menu-right animated--grow-in" role="menu">
+                                        <a class="dropdown-item" role="presentation" href="#">
+                                            <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                                            &nbsp;Profile</a>
+                                        <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item" role="presentation" href="{{ url('/logout') }}">
+                                            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                            &nbsp;Logout
+                                        </a>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+                <div class="container-fluid">
+                    @yield('content')
+                </div>
+            </div>
+            <footer class="bg-white sticky-footer">
+                <div class="container my-auto">
+                    <div class="text-center my-auto copyright"><span>Copyright © Brand 2020</span></div>
+                </div>
+            </footer>
+        </div>
+        <a class="border rounded d-inline scroll-to-top" href="#page-top">
+            <i class="fas fa-angle-up"></i>
+        </a>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js"></script>
+    <script src="{{ asset('assets/js/admin.min.js') }}"></script>
+    @yield('add_script')
+</body>
+
+</html>
