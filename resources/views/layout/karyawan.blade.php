c79cbc713c3ae02cb3e9edd1ae23f04eb56d7099
Adam Syarif Hidayatullah <adamsyarif217@yahoo.com>
2020-04-19 22:42:00 +0700

    Fix copyright

--- resources/views/layout/karyawan.blade.php
+++ resources/views/layout/karyawan.blade.php
@@ -93,7 +93,7 @@
             </div>
             <footer class="bg-white sticky-footer">
                 <div class="container my-auto">
-                    <div class="text-center my-auto copyright"><span>Copyright © Brand 2020</span></div>
+                    <div class="text-center my-auto copyright"><span>Waroeng SS © 2020</span></div>
                 </div>
             </footer>
         </div>
