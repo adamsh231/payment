be32be04455cdf95f9eb6ef7db9e022c26540d21
Adam Syarif Hidayatullah <adamsyarif217@yahoo.com>
2020-04-20 10:45:54 +0700

    Login with Ajax

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,41 +4,43 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
 
     public function authAdmin(Request $request)
     {
-        $messages = [
-            'required' => 'The :attribute field is required.',
-        ];
-        $request->validate([
-            'username' => ['required'],
-            'password' => ['required'],
-        ], $messages);
+        // $messages = [
+        //     'required' => 'The :attribute field is required.',
+        // ];
+        // $request->validate([
+        //     'username' => ['required'],
+        //     'password' => ['required'],
+        // ], $messages);
 
         if (Auth::guard('admin')->attempt($request->only('username', 'password'), $remember = false)) {
-            return redirect('/admin');
+            return response()->json([
+                'error' => false,
+            ], 200);
         } else {
-            return back()->with('status', 'Login Failed! Wrong Email or Password');
+            return response()->json([
+                'error' => true,
+            ], 404);
         }
     }
 
     public function authKaryawan(Request $request)
     {
-        $messages = [
-            'required' => 'The :attribute field is required.',
-        ];
-        $request->validate([
-            'username' => ['required'],
-            'password' => ['required'],
-        ], $messages);
 
         if (Auth::guard('karyawan')->attempt($request->only('username', 'password'), $remember = false)) {
-            return redirect('/karyawan');
+            return response()->json([
+                'error' => false,
+            ], 200);
         } else {
-            return back()->with('status', 'Login Failed! Wrong Email or Password');
+            return response()->json([
+                'error' => true,
+            ], 404);
         }
     }
 
--- resources/views/loginK.blade.php
+++ resources/views/loginK.blade.php
@@ -2,15 +2,17 @@
 @section('title', 'Login Karyawan')
 
 @section('add_style')
+<meta name="csrf-token" content="{{ csrf_token() }}">
 <link rel="stylesheet" href="{{ asset("assets/css/Login-Form-Dark.css") }}">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
 @endsection
 
 @section('body')
 <div class="login-dark" style="height: 730px;">
-    <form action="{{ url('/karyawan') }}" method="POST">
+    <form id="form_login">
         @csrf
         <h1 class="text-center">KARYAWAN</h1>
-        <div class="illustration" style="padding: 0px;"><i class="fa fa-user" style="color: #ffffff;"></i></div>
+        <div class="illustration" style="padding: 0px;"><i class="fa fa-user" style="color: rgb(255,255,255);"></i></div>
         <div class="form-group">
             <input class="form-control text-center" type="text" name="username" placeholder="Username">
         </div>
@@ -18,8 +20,60 @@
             <input class="form-control text-center" type="password" name="password" placeholder="Password">
         </div>
         <div class="form-group">
-            <button class="btn btn-primary btn-block" type="submit">Sign In</button>
+            <a onclick="sigin()" class="btn btn-primary btn-block">Sign In</a>
         </div>
     </form>
 </div>
+@endsection
+
+@section('add_script')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+<script>
+    function sigin(){
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+        $.ajax({
+            type: 'POST',
+            url: '/karyawan',
+            timeout: 2000,
+            data: {
+                username: $('#form_login input[name=username]').val(),
+                password: $('#form_login input[name=password]').val(),
+            },
+            beforeSend: function(){
+                Swal.fire({
+                    title: 'Loading...',
+                    showConfirmButton: false,
+                    allowOutsideClick: false
+                });
+            },
+            success: function(data){
+                Swal.close();
+                window.location.reload();
+            },
+            error: function(data, errortype){
+                console.log(data.responseText);
+                Swal.close();
+                if(errortype == 'timeout'){
+                    Swal.fire({
+                        title: 'Connection Time Out!',
+                        icon: 'warning',
+                        showConfirmButton: true,
+                    });
+                }else{
+                    toastr.options = {
+                        "preventDuplicates": true,
+                        "timeOut": "1000",
+                    };
+                    toastr["error"]("User Not Found! or Mismatch!");
+                }
+
+            }
+        });
+    }
+</script>
 @endsection
